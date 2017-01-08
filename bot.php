498107fafea7f0fa9f7cd416eed4db43fcefd997
eak149 <eak149@gmail.com>
2017-01-09 01:11:15 +0700

    syntax error

--- bot.php
+++ bot.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'vendor/autoload.php';
+
 $access_token = '+0SrTLoZWRp4Fq/dSztfyX+yWwmRLIpIgLTMG0xE/M5hsTqT97/GsXkAXfQW2G8uodEgfbiSuvR4AOnO4CXN3IINjOE7hmGiKA+YIapyxjnjHipS02l0VOjgegMk4j8k52X8BnMcupok7bX4KLK1YgdB04t89/1O/w1cDnyilFU=';
 $secret = 'd84110d1394770c0075d31101d92d588';
 
