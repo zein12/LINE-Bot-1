aab2a9f390e02c46415a193425a57d8660a5e69b
eak149 <eak149@gmail.com>
2017-01-09 01:47:22 +0700

    syntax error

--- bot.php
+++ bot.php
@@ -56,15 +56,15 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				//'text' => $text
+        'text' => $response_format_text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				//'messages' => [$messages],
-        'messages' => $response_format_text;
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
