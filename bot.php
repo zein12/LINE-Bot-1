9ba60713c4f77df36947a94053dcdcfadeb159a4
eak149 <eak149@gmail.com>
2017-01-09 00:41:33 +0700

    add sdk

--- bot.php
+++ bot.php
@@ -1,3 +1,10 @@
 <?php
-echo "bot activated!"
+$access_token = '+0SrTLoZWRp4Fq/dSztfyX+yWwmRLIpIgLTMG0xE/M5hsTqT97/GsXkAXfQW2G8uodEgfbiSuvR4AOnO4CXN3IINjOE7hmGiKA+YIapyxjnjHipS02l0VOjgegMk4j8k52X8BnMcupok7bX4KLK1YgdB04t89/1O/w1cDnyilFU=';
+$secret = 'd84110d1394770c0075d31101d92d588';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);
+
+$response = $bot->replyText('<reply token>', 'hello!');
+
 ?>
--- verified.php
+++ verified.php
@@ -1,5 +1,6 @@
 <?php
-access_token = '+0SrTLoZWRp4Fq/dSztfyX+yWwmRLIpIgLTMG0xE/M5hsTqT97/GsXkAXfQW2G8uodEgfbiSuvR4AOnO4CXN3IINjOE7hmGiKA+YIapyxjnjHipS02l0VOjgegMk4j8k52X8BnMcupok7bX4KLK1YgdB04t89/1O/w1cDnyilFU=';
+$access_token = '+0SrTLoZWRp4Fq/dSztfyX+yWwmRLIpIgLTMG0xE/M5hsTqT97/GsXkAXfQW2G8uodEgfbiSuvR4AOnO4CXN3IINjOE7hmGiKA+YIapyxjnjHipS02l0VOjgegMk4j8k52X8BnMcupok7bX4KLK1YgdB04t89/1O/w1cDnyilFU=';
+
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
