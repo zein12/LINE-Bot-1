eda5183ced5bc6f024a92a4ac717fc5736e222e1
zein12 <jena.zein@gmail.com>
2017-03-13 07:22:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
           $ch1 = curl_init();
           curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
           curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch1, CURLOPT_URL, 'http://id.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles='.$text);
+          curl_setopt($ch1, CURLOPT_URL, 'https://id.wikipedia.org/api.php?action=query&prop=revisions&rvprop=content&format=jsonfm&titles='.$text);
           $result1 = curl_exec($ch1);
           curl_close($ch1);
 
